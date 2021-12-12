3319e44a6ae9d4980bcb7254a21c0873c9f2ecca
Thammika sangkasangwan <Thummika12@gmail.com>
2021-12-12 11:11:36 +0700

    cry

--- work/todolisttable3.py
+++ work/todolisttable3.py
@@ -1,7 +1,17 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets 
 import sqlite3
-from datetime import datetime
+from PyQt5 import QtCore, QtGui, QtWidgets
+import pickle
+import os
+import datetime
+from pprint import pprint
+from google_auth_oauthlib.flow import Flow, InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
+from google.auth.transport.requests import Request
+from datetime import datetime, timedelta
+
 
 
 class Ui_MainWindow(object):
@@ -138,6 +148,8 @@ class Ui_MainWindow(object):
         
         self.addbut.clicked.connect(self.add_task)
         self.clearbut.clicked.connect(self.clear_task)
+        self.delbut.clicked.connect(self.remove_task)
+        self.ggbut.clicked.connect(self.create)
         self.connect_database()
 
 
@@ -166,13 +178,21 @@ class Ui_MainWindow(object):
 
 
 
-        
+    # อยากให้สามารถ เรียงวันที่ได้   
     def add_task(self):
         new_task = self.lineEdit.text()
         new_date = self.lineEdit_2.text()
         if len(new_task) == 0 or len(new_date) == 0:
             self.infolabel.setText("Please enter a task and a date")
             return None
+        if len(new_date)<8:
+            self.infolabel.setText("Incorrect form of date/ leading zero is not allow")
+            return None
+        if new_date.isalpha():
+            self.infolabel.setText("Incorrect form of date")
+            return None
+            
+
     
         self.infolabel.setText("")
         db_cursor.execute("INSERT INTO to_do(task,date) VALUES (:task,:date)",{'task':new_task,'date':new_date})
@@ -183,9 +203,109 @@ class Ui_MainWindow(object):
 
         self.connect_database()
         
+    def remove_task(self):
+        #db_cursor.execute("INSERT INTO sqlite_sequence(seq) VALUES (0)")
+        selected = self.tableWidget.selectedItems()
+        for index in selected:
+            idx = self.tableWidget.item(index.row(),0)
+            idx = idx.text()
+            db_cursor.execute(f"DELETE FROM to_do WHERE id = {idx} ")
+            db_cursor.execute("delete from sqlite_sequence where name='to_do';")
+            db.commit()
+            self.tableWidget.removeRow(index.row())
+        
+    
+     #ปัญหา กด clear แล้ว  หายแค่ตอนแรก add อันอื่นอัเก่ากลับมา
     def clear_task(self):
             self.tableWidget.clearContents()
             self.tableWidget.setRowCount(0)
+     
+     
+    #เริ่ม ggcalrendar button    
+    def create(self):
+        selected = self.tableWidget.selectedItems()
+        
+        def Create_Service(client_secret_file, api_name, api_version, *scopes, prefix=''):
+            CLIENT_SECRET_FILE = client_secret_file
+            API_SERVICE_NAME = api_name
+            API_VERSION = api_version
+            SCOPES = [scope for scope in scopes[0]]
+        
+            cred = None
+            working_dir = os.getcwd()
+            token_dir = 'token files'
+            pickle_file = f'token_{API_SERVICE_NAME}_{API_VERSION}{prefix}.pickle'
+
+            ### Check if token dir exists first, if not, create the folder
+            if not os.path.exists(os.path.join(working_dir, token_dir)):
+                os.mkdir(os.path.join(working_dir, token_dir))
+
+            if os.path.exists(os.path.join(working_dir, token_dir, pickle_file)):
+                with open(os.path.join(working_dir, token_dir, pickle_file), 'rb') as token:
+                    cred = pickle.load(token)
+
+            if not cred or not cred.valid:
+                if cred and cred.expired and cred.refresh_token:
+                    cred.refresh(Request())
+                else:
+                    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+                    cred = flow.run_local_server()
+
+                with open(os.path.join(working_dir, token_dir, pickle_file), 'wb') as token:
+                    pickle.dump(cred, token)
+
+            try:
+                service = build(API_SERVICE_NAME, API_VERSION, credentials=cred)
+                print(API_SERVICE_NAME, API_VERSION, 'service created successfully')
+                return service
+            except Exception as e:
+                print(e)
+                print(f'Failed to create service instance for {API_SERVICE_NAME}')
+                os.remove(os.path.join(working_dir, token_dir, pickle_file))
+                return None
+
+        if __name__ == '__main__':
+            API_NAME = 'calendar'
+            API_VERSION = 'v3'
+            SCOPES = ['https://www.googleapis.com/auth/calendar']
+            CLIENT_FILE = 'client_secret.json'
+            service = Create_Service(CLIENT_FILE, API_NAME, API_VERSION, SCOPES, 'x')
+            
+        CLIENT_SECRET_FILE = "client_secret.json"
+        API_SERVICE_NAME = "calendar"
+        API_VERSION = "v3"
+        SCOPES = ["https://www.googleapis.com/auth/calendar"]
+
+        service = Create_Service(CLIENT_SECRET_FILE, API_SERVICE_NAME, API_VERSION, SCOPES)
+
+
+        #create new events
+         #ปัญหาไม่รู้จะเอา ข้อมูลจากตารางมาใส่ยังไง   
+
+        
+            
+        def create_event(start_time, summary):
+            
+            start_time = start_time
+            end_time = start_time
+            summary = summary
+ 
+            tasks = {
+            'summary': summary,
+            'start': {
+            'date': start_time.strftime("%Y-%m-%d"),
+            'timeZone': 'Asia/Bangkok',
+            },
+            'end': {
+            'date': end_time.strftime("%Y-%m-%d"),
+            'timeZone': 'Asia/Bangkok',
+            },
+            }
+
+            return service.events().insert(calendarId = 'primary', body=tasks).execute()
+            
+        
+        create_event(datetime(2021, 12,28), "python project")
 
 
 
@@ -199,7 +319,7 @@ class Ui_MainWindow(object):
         self.addbut.setText(_translate("MainWindow", "ADD"))
         self.datelabel.setText(_translate("MainWindow", "DATE :"))
         self.tasklabel.setText(_translate("MainWindow", "TASK :"))
-        self.lineEdit_2.setPlaceholderText(_translate("MainWindow", "xx/xx/xxxx"))
+        self.lineEdit_2.setPlaceholderText(_translate("MainWindow", "x/x/xxxx"))
         self.todolistlabel.setText(_translate("MainWindow", "TO DO LIST"))
         item = self.tableWidget.horizontalHeaderItem(0)
         item.setText(_translate("MainWindow", "DATE"))
